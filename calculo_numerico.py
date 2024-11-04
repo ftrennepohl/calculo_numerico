21e76e15339ca9f0f0f9ec005ff4ff0fd825b02e
Fabricio Trennepohl <ftrennepohl@gmail.com>
2024-11-04 18:22:28 -0300

    Método de Gauss

--- calculo_numerico.py
+++ calculo_numerico.py
@@ -27,7 +27,7 @@ def metodoPosicaoFalsa(fun, a, b, epsilon):
             bi = xi
     return fun(ai), fun(bi)
 
-def metodoDasCordas(fun, fun_dx, a, b, x0, epsilon):
+def metodoDasCordas(fun, fun_dx, a, b, epsilon):
     '''
     c = extremo a ser calculado
     d = extremo fixado
@@ -45,6 +45,7 @@ def metodoDasCordas(fun, fun_dx, a, b, x0, epsilon):
     return xi
 
 def metodoDeNewton(fun, fun_dx, a, b, x0, epsilon):
+    # x0: chute inicial
     ai = a
     bi = b
     xi = x0
@@ -63,17 +64,22 @@ def metodoDeNewton(fun, fun_dx, a, b, x0, epsilon):
         return fun(bi), fun(ai)
 
 
-def metodoDaSecante(fun, a, b, x0, epsilon):
+def metodoDaSecante(fun, a, b, x0, x1, epsilon):
+    '''
+    x0, x1: chutes
+    xi: xn-1
+    xj: xn
+    '''
     ai = a
     bi = b
-    x_ant = x0
     xi = x0
+    xj = x1
     while(True):
-        prox_x = ()
+        prox_x = (xi * fun(xj) - x1 * fun(xi)) / (fun(xj) - fun(xi))
         if (criterioParada(prox_x, xi, epsilon)):
             break
-        x_ant = xi
-        xi = prox_x
+        xi = xj
+        xj = prox_x
         if fun(xi) * fun(ai) > 0:
             ai = xi
         else:
@@ -82,3 +88,28 @@ def metodoDaSecante(fun, a, b, x0, epsilon):
         return fun(ai), fun(bi)
     else:
         return fun(bi), fun(ai)
+
+'''
+Sistemas lineares:
+- Métodos diretos: Gauss, Jordan
+- Métodos iterativos: Gauss-Jacobi, Gauss-Seidel
+'''
+
+def metodoDeGauss(a, b, x):
+    # eliminação
+    n = len(a)
+    for k in range(1, n):
+        for i in range(k, n+1):
+            m = a[i][k] / a[k][k]
+            a[i][k] = 0
+            for j in range(k+i, n+1):
+                a[i][j] = a[i][j] - m * a[k][j]
+                b[i] = b[i] - m*b[k]
+    # resolução
+    x[n] = b[n]/a[n][n]
+    for k in range(n-1, 1, -1):
+        s = 0
+        for j in range(k+1, n+1):
+            s = s + a[k][j] * x[j]
+        x[k] = (b[k] - s) / a[k][k]
+
